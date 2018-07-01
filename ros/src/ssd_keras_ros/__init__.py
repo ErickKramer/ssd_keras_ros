2d581d81d501f606e2888d4f39b1539dae5485b4
Minh Nguyen <minhnh.91@gmail.com>
2018-07-01 09:39:19 +0200

    add initial empty ROS package

---
+++ ros/src/ssd_keras_ros/__init__.py
@@ -0,0 +1,1 @@
+from ssd_keras_object_detector import SSDKerasObjectDetector
---
+++ ros/src/ssd_keras_ros/ssd_keras_object_detector.py
@@ -0,0 +1,9 @@
+from mdr_perception_libs import RGBObjectDetector
+
+
+class SSDKerasObjectDetector(RGBObjectDetector):
+    def load_model(self, **kwargs):
+        pass
+
+    def detect_objects(self, image):
+        pass
